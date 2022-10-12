51037fb35f99eb68f873ccd3aa42c8a8fde6aec1
ShreyasGavhalkar <47787863+ShreyasGavhalkar@users.noreply.github.com>
2022-10-12 12:15:31 +0530

    Update a_star.py

--- a_star.py
+++ a_star.py
@@ -1,7 +1,10 @@
+import copy
+
 class puzzle_instance:
     def __init__(self, row, col, state):
         self.space = [row, col]
         self.state = state
+        self.h_score = -1
     def move_right(self):
         if(self.space[1] == 2):
             print("cannot move the space right")
@@ -9,8 +12,7 @@ class puzzle_instance:
             temp = self.state[self.space[0]][self.space[1]+1]
             self.state[self.space[0]][self.space[1]] = temp
             self.state[self.space[0]][self.space[1]+1] = -1
-            self.space = [self.space[0],self.space[1]+1]
-    
+            self.space = [self.space[0],self.space[1]+1]   
     def move_left(self):
         if(self.space[1] == 0):
             print("cannot move the space left")
@@ -38,32 +40,56 @@ class puzzle_instance:
             self.state[self.space[0]+1][self.space[1]] = -1
             self.space = [self.space[0]+1,self.space[1]]
 
+    def get_h_score(self, final_state):
+        h=0
+        if self.state == final_state:
+            return h
+        else:
+            for i in range(len(self.state)):
+                for j in range(len(self.state[i])):
+                    if self.state[i][j] != final_state[i][j]:
+                        h+=1
+        return h
  
 
-def h_score(mat1, mat2):
-    h=0
-    if mat1==mat2:
-        return 0
-    else:
-        for i in range(len(mat1)):
-            for j in range(len(i)):
-                if mat1[i][j] == mat2[i][j]:
-                    continue
-                else:
-                    h+=1
-    return h
+def generate_children(puzzle):
+    state = copy.deepcopy(puzzle.state)
+    space = copy.deepcopy(puzzle.space)
+    temp1 = puzzle_instance(space[0], space[1], state)
+    temp2 = puzzle_instance(space[0], space[1], state)
+    temp3 = puzzle_instance(space[0], space[1], state)
+    temp4 = puzzle_instance(space[0], space[1], state)
+    return []
 
 
 def a_star(puzzle, goal_state):
-    open_list = []
+    breakpoint()
     closed_list = []
     closed_list.append(puzzle)
     g=0
     while(True):
-        h_score = h_score(puzzle.state, goal_state)
-        if h_score  == 0:
+        puzzle.h_score = puzzle.get_h_score(goal_state)
+        if puzzle.h_score  == 0:
             print(f"Reached goal at g={g}")
-            break
+            closed_list.append(puzzle)
+            return closed_list
+        else:
+            children = generate_children(puzzle)
+            g+=1
+            f_score = 999
+            for i in children:
+                i.h_score = i.get_h_score(goal_state)
+                temp = g+i.h_score
+                if temp<f_score:
+                    f_score = temp
+                    puzzle = i
+            closed_list.append(puzzle)
+    return []
+
+
+def show_path(closed_list):
+    for i in closed_list:
+        print(i.state)
 
 
 
@@ -74,17 +100,17 @@ def a_star(puzzle, goal_state):
 if __name__ == '__main__':
     first_state = [
     [1,2,3],
-    [4,5,6],
-    [7,8,-1]
+    [4,-1,6],
+    [7,8,9]
     ]
     #print(first_state)
-    puzzle = puzzle_instance(2,2,first_state)
-    print(puzzle.state)
-    puzzle.move_left()
-    print(puzzle.state)
-    puzzle.move_up()
-    print(puzzle.state)
-    puzzle.move_right()
-    print(puzzle.state)
-    puzzle.move_down()
-    print(puzzle.state)
+    puzzle = puzzle_instance(1,1,first_state)
+    print("*****************************")
+    goal_state = [
+    [1,2,3],
+    [4,8,6],
+    [7,-1,9]
+    ]
+    ans = a_star(puzzle, goal_state)
+    show_path(ans)
+    print("*********************************")
